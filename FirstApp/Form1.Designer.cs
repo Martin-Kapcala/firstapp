a2279acc3b32f5aa8dfd113bc4bb41c392331d84
Martin-Kapcala <martin.kapcala@vitkovickastredni.cz>
2024-11-07 13:12:12 +0100

    Přidejte soubory projektu.

---
+++ FirstApp/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace FirstApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelHead = new System.Windows.Forms.Label();
+            this.buttonSet = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelHead
+            // 
+            this.labelHead.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelHead.Location = new System.Drawing.Point(146, 117);
+            this.labelHead.Name = "labelHead";
+            this.labelHead.Size = new System.Drawing.Size(250, 20);
+            this.labelHead.TabIndex = 0;
+            this.labelHead.Text = "První text...";
+            this.labelHead.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelHead.Click += new System.EventHandler(this.labelHead_Click);
+            // 
+            // buttonSet
+            // 
+            this.buttonSet.Location = new System.Drawing.Point(223, 200);
+            this.buttonSet.Name = "buttonSet";
+            this.buttonSet.Size = new System.Drawing.Size(75, 23);
+            this.buttonSet.TabIndex = 1;
+            this.buttonSet.Text = "Nastavit";
+            this.buttonSet.UseVisualStyleBackColor = true;
+            this.buttonSet.Click += new System.EventHandler(this.buttonSet_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(223, 229);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 2;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(535, 308);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonSet);
+            this.Controls.Add(this.labelHead);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelHead;
+        private System.Windows.Forms.Button buttonSet;
+        private System.Windows.Forms.Button buttonReset;
+    }
+}
+
---
+++ FirstApp/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSet_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+                     
+            int result = random.Next(1, 20);
+
+            labelHead.Text = result.ToString();
+
+        }
+
+        private void labelHead_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            int result = 0;
+
+            labelHead.Text = result.ToString();
+        }
+    }
+}
